4f2b5cb27dc5669ecf1bc17352de98e66efdb82a
riddhi.patel <riddhi.patel@kcsitglobal.com>
2024-03-06 11:54:11 +0530

    Add Chart in Activity slide page
    Add Chart in Activity slide page

--- MagnitudeWebApp/src/app/components/new-slide-page/new-slide-page.component.ts
+++ MagnitudeWebApp/src/app/components/new-slide-page/new-slide-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { AuthService } from 'src/app/services/auth.service';
 import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
 interface IconItem {
   icon: string;
   text: string;
@@ -30,7 +31,8 @@ interface IconItem {
 export class NewSlidePageComponent{
   @Input() image: any;
   @Input() activityName!: string;
-
+  inCountData: number[] = [];
+  outCountData: number[] = [];
   constructor(private route: ActivatedRoute, private router: Router,private service: AuthService,private http: HttpClient) { }
   @Output() close = new EventEmitter<void>();
 
@@ -44,6 +46,7 @@ export class NewSlidePageComponent{
   
       ngOnInit(): void {
         this.service.GetIconMaster().subscribe((fields: IconItem[]) => {
+          debugger
           this.flexItems = fields;
           console.log(fields);
         });
@@ -63,13 +66,80 @@ export class NewSlidePageComponent{
         };
       
         this.http.post<any>('http://webapi.test2.magnitudefb.com/api/Common/GetInOutGraphData', requestData).subscribe(
-          (data) => {
-            console.log(data);
+          (dataArray) => {
+            const data = dataArray[0];
+            this.inCountData = data.InCount ? data.InCount.split(',').map(Number) : [];
+            this.outCountData = data.OutCount ? data.OutCount.split(',').map(Number) : [];
+            this.createChart(this.inCountData, this.outCountData);
           },
           (error) => {
             console.error('Error fetching data:', error);
           }
         );
+      } 
+
+      createChart(inCountData: number[], outCountData: number[]) {
+        var ctx = document.getElementById('myChart') as HTMLCanvasElement | null;
+        
+        // Check if ctx is not null before creating the chart
+        if (ctx) {
+          var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: Array.from({ length: inCountData.length }, (_, i) => (i + 1).toString()), // Generate labels dynamically
+              datasets: [
+                {
+                  label: 'Out Count', // Set label text directly
+                  data: outCountData,
+                  backgroundColor: 'rgba(255, 0, 0, 0.2)', // Change background color to red
+                  borderColor: 'rgba(255, 0, 0, 1)', // Change border color to red
+                  borderWidth: 1,
+                  pointBackgroundColor: 'rgba(255, 0, 0, 1)', // Change point color to red
+                  pointBorderColor: 'rgba(255, 0, 0, 1)' // Change point border color to red
+                },
+                {
+                  label: 'In Count', // Set label text directly
+                  data: inCountData,
+                  backgroundColor: 'rgba(20, 169, 20, 0.2)', // Change background color to green
+                  borderColor: 'rgba(20, 169, 20, 1)', // Change border color to green
+                  borderWidth: 1,
+                  pointBackgroundColor: 'rgba(20, 169, 20, 1)', // Change point color to green
+                  pointBorderColor: 'rgba(20, 169, 20, 1)' // Change point border color to green
+                }
+             ]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  ticks: {
+                    color: 'white' // Color of y-axis labels
+                  }
+                },
+                x: {
+                  ticks: {
+                    color: 'white' // Color of x-axis labels
+                  }
+                }
+              },
+              plugins: {
+                tooltip: {
+                  callbacks: {
+                    labelColor: (context: any) => ({
+                      borderColor: 'white',
+                      backgroundColor: 'white'
+                    })
+                  }
+                }
+              }
+            }
+          });
+        } else {
+          console.error("Could not find canvas element with id 'myChart'");
+        }
       }
       
+      
+      
+  
 }
