ab8d1bc1b54ad9b45104ffd8c00c78052e88ceb0
riddhi.patel <riddhi.patel@kcsitglobal.com>
2024-03-11 15:11:26 +0530

    Add Captureform Data into DB
    Add Captureform Data into DB.

--- Backend_mg/Backend_mg/Controllers/FormResponsesController.cs
+++ Backend_mg/Backend_mg/Controllers/FormResponsesController.cs
@@ -21,23 +21,50 @@ namespace Backend_mg.Controllers
         {
             return await _context.FormResponses.ToListAsync();
         }
+        //[HttpPost]
+        //public async Task<ActionResult<FormResponses>> PostFormResponses(FormResponses contactInformation)
+        //{
+        //    try
+        //    {
+        //        DateTime dateTime = DateTime.Now;
+        //        contactInformation.submission_date = dateTime;
+        //        _context.FormResponses.Add(contactInformation);
+        //        await _context.SaveChangesAsync();
+
+        //        return CreatedAtAction(nameof(GetFormResponses), contactInformation);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Log the exception
+        //        Console.Error.WriteLine(ex);
+        //        return StatusCode(500, "Internal Server Error");
+        //    }
+        //}
         [HttpPost]
-        public async Task<ActionResult<FormResponses>> PostFormResponses(FormResponses contactInformation)
+        public IActionResult AddCapture(List<FormResponses> formResponses)
         {
             try
             {
-                DateTime dateTime = DateTime.Now;
-                contactInformation.submission_date = dateTime;
-                _context.FormResponses.Add(contactInformation);
-                await _context.SaveChangesAsync();
+           
+                int formId = 1;
+                int fieldId = 1;
 
-                return CreatedAtAction(nameof(GetFormResponses), contactInformation);
+                foreach (var response in formResponses)
+                {
+                    response.form_id = formId;
+                    response.field_id = fieldId;
+                    response.submission_date = DateTime.Now;
+
+                    _context.FormResponses.Add(response);
+
+                    _context.SaveChanges();
+                }
+
+                return Ok(new { message = "Form responses added successfully." });
             }
             catch (Exception ex)
             {
-                // Log the exception
-                Console.Error.WriteLine(ex);
-                return StatusCode(500, "Internal Server Error");
+                return StatusCode(500, new { error = $"An error occurred: {ex.Message}" });
             }
         }
     }
